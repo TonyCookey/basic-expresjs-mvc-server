7b6bd256f0cd36c0e0b7bec2ae15aabfac9dd881
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-02-02 20:43:38 +0100

    added base url to logging middleware

--- middlewares/log.middleware.js
+++ middlewares/log.middleware.js
@@ -2,7 +2,7 @@ function logIncomingRequests(req, res, next) {
     const start = Date.now()
     next()
     const delta = Date.now() - start
-    console.log(`${req.method}: ${req.url} , ${delta}ms`);
+    console.log(`${req.method}: ${req.baseUrl} ${req.url} , ${delta}ms`);
 }
 
 module.exports = logIncomingRequests
